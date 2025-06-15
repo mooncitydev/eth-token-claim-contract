a65c6ca919dfbb12040ffa4d81e244ea3360cf83
mooncitydev <mooncity.james@gmail.com>
2025-06-15 16:18:48 -0700

    establish module layout for domain boundaries

---
+++ contracts/Claim.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Claim is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    // The ERC20 token to be transferred
+    IERC20 public token;
+
+    // Backend wallet address that signs the messages
+    address public backendWallet;
+
+    // Track if a signed message has been used (to prevent reuse of same signature)
+    mapping(bytes32 => bool) public usedSignatures;
+
+    // Events
+    event TokensClaimed(
