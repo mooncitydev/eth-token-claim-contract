4a72ff6385d5dbba1b51da0dcf03cc968fe60ded
mooncitydev <mooncity.james@gmail.com>
2025-09-12 16:50:30 -0700

    split monolith handlers into domain modules

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,3 +6,5 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
