b5e93329b6d7055a5027a6a3f350f545224cbaf1
mooncitydev <mooncity.james@gmail.com>
2025-10-03 01:45:38 -0700

    simplify control flow in application modules

--- contracts/MockERC20.sol
+++ contracts/MockERC20.sol
@@ -10,3 +10,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockERC20 is ERC20 {
     constructor(
         string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
