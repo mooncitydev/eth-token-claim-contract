2e43c29fd8f513e5ff564a8bd508f1b8b6084730
mooncitydev <mooncity.james@gmail.com>
2026-01-21 10:51:36 -0800

    correct edge case handling in application modules

--- script/deployClaim.js
+++ script/deployClaim.js
@@ -49,3 +49,7 @@ async function main() {
 main()
     .then(() => process.exit(0))
     .catch((error) => {
+        console.error("\n❌ Deployment failed:");
+        console.error(error);
+        process.exit(1);
+    });
